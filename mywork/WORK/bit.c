a277c8d36ed122ecff1b8a9d6ae0349b51764f3a
HFYkratos <402853094@qq.com>
2019-05-29 11:22:38 +0800

    作业习题

---
+++ mywork/WORK/bit.c
@@ -0,0 +1,861 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<stdlib.h>
+//#include<math.h>
+//#include<ctype.h>
+
+
+
+												  //DAY 10
+//int main()
+//{
+//	int i, j, sz;
+//	int arr[5][5] = { 0 };
+//	sz = sizeof(arr) / sizeof(arr[0]);
+//	for ( i = 0; i <= sz; i++)
+//	{
+//		arr[i][0] = 1;
+//		arr[i][i] = 1;
+//	}
+//	for ( i = 2; i < sz; i++)
+//	{
+//		for ( j = 1; j <= i; j++)
+//		{
+//			arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
+//		}
+//	}
+//	for ( i = 0; i < sz; i++)
+//	{
+//		for ( j = 0; j < sz - i; j++)
+//		{
+//			printf(" ");
+//		}
+//		for ( j = 0; j <= i; j++)
+//		{
+//			printf("%d ", arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//	system("pause");
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0; i < 4; i++)
+//	{
+//		int killer = 65 + i;
+//		j = ((killer != 'A') + (killer == 'C') + (killer == 'D') + (killer != 'D'));
+//		if (j == 3)
+//		{
+//			printf("杀人凶手是%c\n", killer);
+//			break;
+//		}
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int A, B, C, D, E;
+//	for ( A = 0; A <= 5; A++)
+//	{
+//		for ( B = 0; B <= 5; B++)
+//		{
+//			for (C = 0; C <= 5; C++)
+//			{
+//				for (D = 0; D <= 5; D++)
+//				{
+//					for (E = 0; E <= 5; E++)
+//					{
+//						if ((B == 2 && A != 3) || (B != 2 && A == 3) == 1)
+//						{
+//							if ((B == 2 && E != 4 ) || (B != 2 && E == 4) == 1)
+//							{
+//								if ((C == 1 && D != 2) || (C != 1 && D == 2) == 1)
+//								{
+//									if ((C == 5 && D != 3) || (C != 5 && D == 3) == 1)
+//									{
+//										if ((E == 4 && A != 1) || (E != 4 && A == 1) == 1)
+//										{
+//											if (120 == A * B * C * D * E)
+//											{
+//												printf("A=%d B=%d C=%d D=%d E=%d\n", A, B, C, D, E);
+//											}
+//										}
+//									}
+//								}
+//							}
+//						}
+//					}
+//				}
+//			}
+//		}
+//	}
+//	system("pause");
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int arr1[32] = { 0 };
+//	int arr2[32] = { 0 };
+//	int x, y, i, n = 0;
+//	scanf("%d.%d", &x, &y);
+//	for ( i = 0; i < 32; i++)
+//	{
+//		arr1[i] = x & 1;
+//		x = x >> 1;
+//		arr2[i] = y & 1;
+//		y = y >> 1;
+//	}
+//	for ( i = 0; i < 32; i++)
+//	{
+//		if (arr1[i] != arr2[i])
+//		{
+//			n++;
+//		}
+//	}
+//	printf("%d\n", n);
+//	system("pause");
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int num = 0;
+//	int arr[32] = { 0 };
+//	int len = 0;
+//	int i;
+//	scanf("%d", &num);
+//	int tmp = num;
+//	while((num % 10) > 0)
+//	{
+//				len++;
+//				num = num / 10;
+//    }
+//	num = tmp;
+//	for ( i = 0; i <= len; i++)
+//	{
+//		arr[i] = num % 10;
+//		num = num / 10;
+//	}
+//	for (i = len - 1; i >= 0; i--)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	printf("\n");
+//	system("pause");
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int i, j = 0;
+//	int arr1[16];
+//	int arr2[16];
+//	int num = 0;
+//	scanf("%d", &num);
+//	for ( i = 0; i < 32; i += 2)
+//	{
+//		arr1[j] = ((num >> i) & 1);
+//		j++;
+//	}	for (i = 1,j = 0; i < 32; i += 2)
+//	{
+//		arr2[j] = ((num >> i) & 1);
+//		j++;
+//	}
+//	printf("奇数位为：");
+//	for (i = 15; i >= 0; i--)
+//	{
+//		printf("%d ", arr1[i]);
+//	}
+//	printf("\n偶数位为：");
+//	for (i = 15; i >= 0; i--)
+//	{
+//		printf("%d ", arr2[i]);
+//	}
+//	printf("\n");
+//	system("pause");
+//	return 0;
+//}
+//
+//int count_one_bits(unsigned int num)
+//{
+//	int n = 0;
+//	while (num > 0)
+//	{
+//		if (1 == num % 2)
+//		{
+//			n++;
+//		}
+//		num = num >> 1;
+//	}
+//	return n;
+//}
+//int main()
+//{
+//	int num = 0;
+//	scanf("%d", &num);
+//	printf("%d\n", count_one_bits(num));
+//	system("pause");
+//	return 0;
+//}
+
+//                                            DAY 07
+//
+//void gewei(int n)
+//{
+//	if (n > 9)
+//	{
+//		gewei(n / 10);
+//	}
+//	printf("%d ", n % 10);
+//	printf("\n");
+//}
+//int main()
+//{
+//	gewei(1234);
+//	system("pause");
+//	return 0;
+//}
+//
+//int jiecheng2(int n)//非递归形式
+//{
+//	int i, m = 1;
+//	for ( i = 1; i <= n; i++)
+//	{
+//		m = m * i;
+//	}
+//	return m;
+//}
+//int jiecheng1(int n)//递归形式
+//{
+//	if (n > 1)
+//	{
+//		return n * jiecheng1(n - 1);
+//	}
+//	return 1;
+//}
+//int main()
+//{
+//	printf("%d\n", jiecheng2(5));
+//	system("pause");
+//	return 0;
+//}
+//
+//int mystrlen2(char* x)//非递归形式
+//{
+//	int count = 0;
+//	while (*x != 0)
+//	{
+//		count++;
+//		x++;
+//	}
+//	return count;
+//}
+//int mystrlen1(char* x)//递归形式
+//{
+//	if (*x == 0)
+//	{
+//		return 0;
+//	}
+//	return 1 + mystrlen1(x + 1);
+//}
+//int main()
+//{
+//	printf("%d\n", mystrlen1("huangfengyun"));
+//	system("pause");
+//	return 0;
+//}
+//
+//int DigitSum(int n)
+//{
+//	if (n)
+//	{
+//		return n % 10 + DigitSum(n / 10);
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int n;
+//	scanf("%d", &n);
+//	printf("%d\n", DigitSum(n));
+//	system("pause");
+//	return 0;
+//}
+
+//int cf(int n, int k)
+//{
+//	int res;
+//	if (k >= 1)
+//		return res = n * cf(n,k - 1);
+//	if (k == 0)
+//		return 1;
+//}
+//int main()
+//{
+//	int n, k, res;
+//	scanf("%d %d", &n, &k);
+//	res = cf(n, k);
+//	printf("%d\n", res);
+//	system("pause");
+//	return 0;
+//}
+
+//int fib1(int n)//递归形式
+//{
+//	int result;
+//	if (n > 2)
+//	{
+//		return result = fib1(n - 1) + fib1(n - 2);
+//	}
+//	if (n <= 2)
+//		return 1;
+//}
+//int fib2(int n)//非递归形式
+//{
+//	int result;
+//	int pre_result;
+//	int next_older_result;
+//	result = pre_result = 1;
+//	while (n > 2)
+//	{
+//		n -= 1;
+//		next_older_result = pre_result;
+//		pre_result = result;
+//		result = pre_result + next_older_result;
+//	}
+//	return result;
+//}
+//int main()
+//{
+//	int n, result;
+//	printf("请输入需要求得的第n个数：");
+//	scanf("%d", &n);
+//	result = fib1(n);
+//	printf("%d\n", result);
+//	system("pause");
+//	return 0;
+//}
+
+//void init(int* a[],int sz)
+//{
+//	int i = 0;
+//	for ( i = 0; i < sz; i++)
+//	{
+//		scanf("%d", &a[i]);
+//	}
+//}
+//void empty(int* a[],int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		a[i] = 0;
+//	}
+//}
+//void reverse(int* a[],int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i <= sz; i++)
+//	{
+//		a[i] = a[sz - 1 - i];
+//	}
+//}
+//int main()
+//{
+//	int i = 0;
+//	int sz = 0;
+//	int arr[10] = { 0 };
+//	sz = sizeof(arr) / sizeof(arr[0]);
+//	init(arr, sz);
+//	for ( i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	printf("\n");
+//	reverse(arr, sz);
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	printf("\n");
+//	empty(arr, sz);
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	printf("\n");
+//	system("pause");
+//	return 0;
+//}
+//
+//int num(int a) 
+//{
+//	int i = 0;
+//	for ( i = 2; i < sqrt(a); i++)
+//	{
+//		if (a%i == 0)
+//		{
+//			return 0;
+//		}
+//	}
+//	return 1;
+//}
+//int main()
+//{
+//	int n, m;
+//	for ( n = 100; n < 200; n++)
+//	{
+//		int m = num(n);
+//		if (m == 1)
+//		{
+//			printf("%d ", n);
+//		}
+//	}
+//	system("pause");
+//	return 0;
+//}
+//
+//int is_leap_year(int y)
+//{
+//	if (y % 4 == 0 && y % 100 != 0 || y % 400 == 0)
+//	{
+//		return 1;
+//	}
+//	else
+//	{
+//		return 0;
+//	}
+//}
+//int main()
+//{
+//	int year = 1;
+//	scanf("%d", &year);
+//	int i = is_leap_year(year);
+//	if (i == 1)
+//	{
+//		printf("是闰年。\n");
+//	}
+//	else
+//	{
+//		printf("不是闰年。\n");
+//	}
+//	system("pause");
+//	return 0;
+//}
+//
+//
+//void exchange(int* a, int* b)
+//{
+//	int tmp;
+//	tmp = *a;
+//	*a = *b;
+//	*b = tmp;
+//}
+//
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	printf("a=%d , b=%d\n", a, b);
+//	exchange(&a, &b);
+//	printf("a=%d , b=%d\n", a, b);
+//	system("pause");
+//	return 0;
+//}
+//
+//
+//int main()
+//{
+//	int x, y;
+//	int n = 0;
+//	scanf("%d", &n);
+//	for ( x = 1; x <= n; x++)
+//	{
+//		for ( y = 1; y <= x; y++)
+//		{
+//			printf(" %d*%d = %d ", x, y, x*y);
+//		}
+//		printf("\n");
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int k = 0;
+//	printf("输入想要查找的下标(小于10)：");
+//	scanf("%d", &k);
+//	int left = arr[0];
+//	int right = sizeof(arr) / sizeof(arr[0]) - 1;
+//	while (left <= right)
+//	{
+//		int mid = (left + right) / 2;
+//		if (arr[mid] == k)
+//		{
+//			printf("找到了！下标为：%d", mid);
+//			break;
+//		}
+//		else if (arr[mid] < k)
+//		{
+//			left = mid + 1;
+//		}
+//		else
+//		{
+//			right = mid - 1;
+//		}
+//	}
+//	if (left > right)
+//	{
+//		printf("找不到！");
+//	}
+//	system("pause");
+//	return 0;
+//}
+//
+//int main()
+//{
+//	char number;
+//	while (1)
+//	{
+//		scanf("%c", &number);
+//		if (number >= 'A' && number <= 'Z')
+//		{
+//			printf("%c", number + 32);
+//		}
+//		if (number >= 'a'&&number <= 'z')
+//		{
+//			printf("%c", number - 32);
+//		}
+//	}
+//	system("pause");
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int pw = 123;
+//	int i, n = 0;
+//	printf("请输入密码：");
+//	for ( i = 0; i < 3; i++)
+//	{
+//		scanf("%d", &n);
+//		if (pw != n)
+//		{
+//			printf("密码错误！\n");
+//		}
+//		if (pw == n)
+//			break;
+//	}
+//	if (i == 3)
+//	{
+//		printf("密码错误达到三次，退出程序\n");
+//	}
+//	if (i < 2)
+//	{
+//		printf("密码正确！");
+//	}
+//	system("pause");
+//	return 0;
+//}
+//
+//int main() 
+//{
+//	int num, g;
+//	srand(time(NULL));
+//	num = rand() % 100;
+//	do
+//	{
+//		scanf("%d", &g);
+//		if (g > num)
+//		{
+//			printf("大了\n");
+//		}
+//		if (g < num)
+//		{
+//			printf("小了\n");
+//		}
+//	} while (num != g);
+//	printf("猜对了\n");
+//	system("pause");
+//	return 0;
+//}
+
+//int main()
+//{
+//int  i = 0;
+//int  j = 0;
+//for (i = 0; i < 7; i++)
+//	{
+//		for (j = 0; j < 2 * i + 1; j++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//for (i = 0; i < 6; i++)
+//{
+//		for (j = 0; j < 11 - 2 * i; j++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//}
+//	system("pause");
+//	return 0;
+//}
+//
+//int main() 
+//{
+//	int x, i;
+//	int a, b, c;
+//	for (i = 100; i <= 1000; i++)
+//	{
+//		if (i%100 != 0)
+//		{
+//			a = i / 100;
+//			b = i % 100/10;
+//			c = i % 10;
+//			x = (a * a * a) + (b * b * b) + (c * c * c);
+//			if ( x == i)
+//			{
+//				printf("%d\n", i);
+//			}
+//		}
+//	}
+//	system("pause");
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int i, a, b, c, d, e, sum;
+//	scanf("%d", &i);
+//	a = i;
+//	b = a + 10 * i;
+//	c = b + 100 * i;
+//	d = c + 1000 * i;
+//	e = d + 10000 * i;
+//	sum = a + b + c + d + e;
+//	printf("%d", sum);
+//	system("pause");
+//	return 0;
+//}
+
+//int main() {
+//	int x, n;
+//	for (x = 100; x <= 200; x++)
+//	{
+//		for (n = 2; n <= x; n++)
+//		{
+//			if (x%n == 0)
+//				break;
+//		}
+//		if (x == n)
+//			printf("%d ", x);
+//	}
+//	system("pause");
+//	return 0;
+//}
+//
+//
+//int main() {
+//	int x, y;
+//	for(x=1;x<=9;x++)
+//	{
+//		for (y = 1; y <= 9; y++)
+//		{
+//			printf("%d*%d=%d\n", x, y, x*y);
+//	}
+//	}
+//	system("pause");
+//	return 0;
+//}
+//
+//
+//
+//int main() {
+//	int c,a;
+//	printf("请随意输入一年：");
+//	scanf("%d",&c);
+//	if ((c % 4 == 0 %  c % 100 != 0) || c & 400 == 0)
+//	a = 1;
+//	else
+//		a = 0;
+//	if (a != 0)
+//		printf("%d是闰年\n", c);
+//	else
+//		printf("%d不是闰年\n", c);
+//	system("pause");
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int x, y, i;
+//	scanf("%d %d", &x, &y);
+//	i = x;
+//	x = y;
+//	y = i;
+//	printf("%d %d", x, y);
+//	system("pause");
+//	return 0;
+//}
+//
+//int main() 
+//{
+//	int x, y;
+//	scanf("%d %d", &x, &y);
+//	x = x + y;
+//	y = x - y;
+//	x = x - y;
+//	printf("%d %d", x, y);
+//	system("pause");
+//	return 0;
+//}
+//
+//int main() {
+//	int x, max, i;
+//	printf("请输入10个整数：\n");
+//	scanf("%d", &x);
+//	max = x;
+//	for(i=1;i<10;i++)
+//	{
+//		scanf("%d", &x);
+//		if (max < x)
+//		{
+//			max = x;
+//		}
+//	}
+//	printf("最大的数为：%d", max);
+//	system("pause");
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int x, y, z, t;
+//	scanf("%d%d%d", &x, &y, &z);
+//	if (x < y)
+//	{
+//		t = x;
+//		x = y;
+//		y = t;
+//	}
+//	if (y < z)
+//	{
+//		t = y;
+//		y = z;
+//		z = t;
+//	}
+//	if (x < z)
+//	{
+//		t = x;
+//		x = z;
+//		z = t;
+//	}
+//	printf("%d %d %d", x, y, z);
+//	system("pause");
+//	return 0;
+//}
+//
+//int main() 
+//{
+//	int x, y, i,m;
+//	scanf("%d %d", &x, &y);
+//	if (x > y)
+//		m = y;
+//	else
+//	{
+//		m = x;
+//	}
+//	for (i = m; i > 1; i--)
+//	{
+//	   if (x%i == 0 && y%i == 0)
+//	   break;
+//	}
+//	printf("%d ", i);
+//    system("pause");
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int i, j, k;
+//	int A[10];
+//	int B[10];
+//	int C[10];
+//	printf("请输入10个数给A组：");
+//	for (i = 0; i < 10; i++)
+//	{
+//		scanf("%d", &A[i]);
+//	}
+//	printf("请输入10个数给B组：");
+//	for (j = 0; j < 10; j++)
+//	{
+//		scanf("%d", &B[j]);
+//	}
+//	for (k = 0; k < 10; k++)
+//	{
+//		C[k] = A[k]+B[k];
+//		B[k] = C[k]-B[k];
+//		A[k] = C[k]-A[k];
+//	}
+//	printf("A组：");
+//	for (j = 0; j < 10; j++)
+//	{
+//		printf("%d ", A[j]);
+//	}
+//	printf("\n");
+//	printf("B组：");
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", B[i]);
+//	}
+//	printf("\n");
+//	system("pause");
+//	return 0;
+//}
+//
+//int main()
+//{
+//	double n, i;
+//	double s=0.0;
+//	for ( i = 1; i <= 99; i++)
+//	{
+//		s = s + 1 / i;
+//	}
+//	for ( n = 2; n <= 100 ; n++)
+//	{
+//		s = s - 1 / n;
+//	}
+//	printf("%lf\n", s);
+//	system("pause");
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int i, n;
+//	int coun = 0;
+//	for ( n = 1; n <= 100; n++)
+//	{
+//		i = n % 10;
+//		if (9 == i)
+//		{
+//			coun++;
+//		}
+//	}
+//	printf("%d\n", coun);
+//	system("pause");
+//	return 0;
+//}
---
+++ mywork/WORK/school.c
@@ -0,0 +1,711 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<ctype.h>
+#include<time.h>
+#include<string.h>
+
+//5.23
+
+//double mymax(double x, double y)
+//{
+//	if (x >= y)
+//	{
+//		return x;
+//	}
+//	else
+//	{
+//		return y;
+//	}
+//}
+//double mymin(double x, double y)
+//{
+//	if (x >= y)
+//	{
+//		return y;
+//	}
+//	else
+//	{
+//		return x;
+//	}
+//}
+// int main()
+//{
+//	printf("%lf\n", mymax(15, 12));
+//	printf("%lf\n", mymin(5, 12));
+//	system("pause");
+//	return 0;
+//}
+//
+// int fun(int n)
+// {
+//	 if (n > 0)
+//	 {
+//		 return n * fun(n - 1);
+//
+//	 }
+//	 return 1;
+// }
+// int main()
+// {
+//	 int n;
+//	 scanf("%d", &n);
+//	 printf("%d\n", fun(n));
+//	 system("pause");
+//	 return 0;
+// }
+//
+//int fun(int n)
+//{
+//	if (n > 0)
+//	{
+//		return n + fun(n - 1);
+//	}
+//	else
+//	{
+//		return 0;
+//	}
+//	
+//}
+//int main()
+//{
+//	int n;
+//	scanf("%d", &n);
+//	printf("%d\n", fun(n));
+//	system("pause");
+//	return 0;
+//}
+//
+//void fun(int n)
+//{
+//	int x, y;
+//	for (x = 1; x <= n; x++)
+//	{
+//		for (y = 1; y <= x; y++)
+//		{
+//			printf(" %d*%d = %d ", y, x, x*y);
+//		}
+//		printf("\n");
+//	}
+//}
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	fun(n);
+//	system("pause");
+//	return 0;
+//}
+//
+//double myfsqrt(double a)
+//{
+//	int i;
+//	int x, x2, x1;
+//	x = a / 2.0;
+//	for (i = 0; ; i++)
+//	{
+//		x1 = (x + a / x) / 2;
+//		x2 = x - x1;
+//		if (fabs(x2) < 1e-5)
+//			break;
+//		x = x1;
+//	}
+//	return x1;
+//}
+//int main()
+//{
+//	double a;
+//	scanf("%lf", &a);
+//	myfsqrt(a);
+//	printf("%lf", myfsqrt(a));
+//	system("pause");
+//	return 0;
+//}
+
+
+//                                       WORK 4
+//int main()
+//{
+//	int arr[4][4] = { 0 };
+//	int i, j, k = 0;
+//	int res = 1;
+//	for (i = 0; i < 4; i++)
+//	{
+//		for (j = 0; j < 4; j++)
+//		{
+//			scanf("%d", &arr[i][j]);
+//		}
+//	}
+// 	for ( i = 0; i <= 3; i++)
+//	{
+//		for (j = 3; j >= k; j--)
+//		{
+//			if (arr[i][j] != 0)
+//			{
+//				res = 0;
+//			}
+//			res = res * 1;
+//		}
+//		k++;
+//	}
+//	if (res == 1)
+//	{
+//		printf("YES!\n");
+//	}
+//	if (res == 0)
+//	{
+//		printf("NO!\n");
+//	}
+//	system("pause");
+//	return 0;
+//}
+//
+//int main() 
+//{
+//	int A[5][4] = { 0 };
+//	int x[5] = { 0 };
+//	int i, j, k = 0;
+//	for (i = 0; i < 5; i++)
+//	{
+//		for (j = 0; j < 4; j++)
+//		{
+//			scanf("%d", &A[i][j]);
+//		}
+//	}
+//	for (i = 0; i <= 4; i++)
+//	{
+//		for ( j = 0; j <= 3; j++)
+//		{
+//			x[k] = x[k] + A[i][j];
+//		}
+//		k++;
+//	}
+//	for (i = 0; i <= 4; i++)
+//	{
+//		for (j = 0; j <= 3; j++)
+//		{
+//			printf("%d ", A[i][j]);
+//		}
+//		printf("%d\n", x[i]);
+//	}
+//	system("pause");
+//	return 0;
+//}
+//
+//struct vote 
+//{
+//	char name[5];
+//	int count;
+//}s[5] = { "A",0,"B",0,"C",0 ,"D",0 ,"E",0 };
+//
+//int main()
+//{
+//	int num, max = 0;
+//	char vote[50];
+//	int i, n, j = 0, k = 0, m = 0;
+//	printf("请输入候选人人数：\n");
+//	scanf("%d", &num);
+//	printf("请输入候选人名字：\n");
+//	for (i = 0; i < num; i++)
+//	{
+//		scanf("%s", s[i].name);
+//	}
+//	printf("请输入投票人数：\n");
+//	scanf("%d", &n);
+//	for (i = 0; i < n; i++)
+//	{
+//		printf("No.%d已经投票，名字是:", i + 1);
+//		scanf("%s", &vote[i]);
+//		for (j = 0; j < num; j++) {
+//			if (strcmp(s[j].name , vote) == 0)
+//			{
+//				s[j].count++;
+//				k = s[j].count;
+//			}
+//			if (max < k)
+//			{
+//				max = k;
+//				m = j;
+//			}
+//		}
+//	}
+//	printf("投票结束\n");
+//	for (i = 0; i < num; i++)
+//	{
+//		printf("%s的得票数为%d\n", s[i].name, s[i].count);
+//	}
+//	printf("优胜者是%s!!!\n得票数为%d\n", s[m].name, max + 1);
+//	system("pause");
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int arr[15] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 };
+//	int n = 0;
+//	int i = 0;
+//	scanf("%d", &n);
+//	for (i = 0; i < 15; i++)
+//	{
+//		arr[(15 + i) % n] = arr[i];
+//	}
+//	for (i = 0; i < 15; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	system("pause");
+//	return 0;
+//}
+//
+//int main() 
+//{
+//	int ten = 0;
+//	int two = 0;
+//	printf("请输入一个十进制整数.\n");
+//	scanf("%d", &ten);
+//	int a[32];
+//	while ( ten > 0)
+//	{
+//		a[two] = ten % 2;
+//		two = two + 1;
+//		ten = ten / 2;
+//	}
+//	printf("十进制整数转换为二进制数是:\n");
+//	for (two-- ; two >= 0; two--)
+//	{
+//		printf("%d", a[two]);
+//	}
+//	printf("\n");
+//	system("pause");
+//	return 0;
+//}
+
+//int main()
+//{
+//	int x, i, y;
+//	int a, b, c;
+//	for (i = 100; i <= 1000; i++)
+//	{
+//		a = i / 100;
+//		b = i % 100 / 10;
+//		c = i % 10;
+//		x = a * b*c;
+//		y = a + b + c;
+//		if (x > y)
+//		{
+//			printf("%d\n", i);
+//		}
+//	}
+//	system("pause");
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int i = 0, space = 0, num = 0, n = 0, ch = 0;
+//	char s[80];
+//	printf("请输入一串字符 ");
+//	gets(s);
+//	while (s[i] != '\0')
+//	{
+//		if (s[i] == ' ')
+//			space++;
+//		else if (s[i] <= '9' && s[i] >= '0')
+//			num++;
+//		else if (s[i] <= 'z' && s[i] >= 'a' || s[i] <= 'Z' && s[i] >= 'A')
+//			ch++;
+//		else
+//			n++;
+//		i++;
+//	}
+//	printf("刚才输入的字符中英文字符个数为 %d\n", ch);
+//	printf("刚才输入的字符中空格个数为 %d\n", space);
+//	printf("刚才输入的字符中数字个数为 %d\n", num);
+//	printf("刚才输入的字符中其他个数为 %d\n", n);
+//	system("pause");
+//	return 0;
+//}
+//int main()
+//{
+//	double x1, a, x2, d;
+//	scanf("%lf", &a);
+//	x1 = a;
+//	do
+//	{
+//		x2 = 0.5*(x1 + a / x1);
+//		d = x1 * x2;
+//		x1 = x2;
+//	} while (d*d < 0.0001);
+//	printf("%lf\n", x1);
+//	system("pause");
+//	return 0;
+//}
+
+
+
+//int sum()
+//{
+//	int num1, num2, num;
+//	srand(time(NULL));
+//	do
+//	{
+//		num1 = rand() % 100;
+//		num2 = rand() % 100;
+//		num = num1 + num2;
+//	} while (num>100);
+//	printf("%d+%d=", num1, num2);
+//	return num;
+//}
+//int jian()
+//{
+//	int num1, num2, num;
+//	srand(time(NULL));
+//	do
+//	{
+//		num1 = rand() % 100;
+//		num2 = rand() % 100;
+//		num = num1 - num2;
+//	} while (num > 100 || num < 0);
+//	printf("%d-%d=", num1, num2);
+//	return num;
+//}
+//int main()
+//{
+//	int y, x, result, i, n;
+//	int z = 1;
+//	srand(time(NULL));
+//	printf("请选择加减法:\n1——加法\n2——减法\n其他——加减法混合\n");
+//	scanf("%d", &i);
+//	do
+//	{
+//		if (i == 1)
+//		{
+//			for (n = 0; n < 10; n++)
+//			{
+//				result = sum();
+//				scanf("%d", &y);
+//				if (y == result)
+//				{
+//					printf("回答正确！\n");
+//					system("pause");
+//				}
+//				else
+//				{
+//					printf("回答错误！正确答案为%d\n", result);
+//					system("pause");
+//				}
+//			}
+//		}
+//		if (i == 2)
+//		{
+//			for (n = 0; n < 10; n++)
+//			{
+//				result = jian();
+//				scanf("%d", &y);
+//				if (y == result)
+//				{
+//					printf("回答正确！\n");
+//					system("pause");
+//				}
+//				else
+//				{
+//					printf("回答错误！正确答案为%d\n", result);
+//					system("pause");
+//				}
+//			}
+//		}
+//
+//		else
+//		{
+//			for (n = 0; n < 10; n++)
+//			{
+//				x = rand() % 2;
+//				switch (x)
+//				{
+//				case 0:result = sum(); break;
+//				case 1:result = jian(); break;
+//				}
+//				scanf("%d", &y);
+//				if (y == result)
+//				{
+//					printf("回答正确！\n");
+//					system("pause");
+//				}
+//				else
+//				{
+//					printf("回答错误！正确答案为%d\n", result);
+//					system("pause");
+//				}
+//			}
+//		}
+//		printf("是否重新再计算一遍：\n1——重新\n2——结束");
+//		scanf("%d", &z);
+//		if (z != 1)
+//		{
+//			break;
+//		}
+//	} while (z = 1);
+//	system("pause");
+//	return 0;
+//}
+
+
+
+
+////int main()
+////{
+////	int x, i, min;
+////	printf("请输入12个数据：\n");
+////	scanf("%d", &x);
+////	min = x;
+////	for (i = 2; i <= 12; i++)
+////		{
+////			scanf("%d", &x);
+////			if (x < min)
+////			{
+////				min = x;
+////			}
+////		}
+////	printf("输入数据中最小的数：%d\n", min);
+////	system("pause");
+////	return 0;
+////}
+////
+////
+////
+////int main()
+////{
+////	int x=0, i=1;
+////	scanf("%d", &x);
+////	for (; x > 0;x--)
+////	{
+////		i = x * i;
+////	}
+////	printf("%d\n", i);
+////	system("pause");
+////	return 0;
+////}
+////
+////
+////
+////
+////
+////int main()
+////{
+////	int x, y, i,m;
+////	scanf("%d %d", &x, &y);
+////	if (x > y)
+////		m = y;
+////	else
+////	{
+////		m = x;
+////	}
+////	for (i = m; i <= m; i++)
+////	{
+////		if (x%i == 0 && y%i == 0)
+////		{
+////			printf("%d ", i);
+////		}break;
+////	}
+////	system("pause");
+////	return 0;
+////}
+////
+////int main()
+////{
+////	int x1, x2, x3, y1, y2, y3 ;
+////	double S ,a, b, c, q ,k1, k2, k3;
+////	printf("请输入指定坐标:");
+////	scanf("%d %d",&x1,&y1);
+////	printf("请输入指定坐标:");
+////	scanf("%d %d",&x2,&y2);
+////	printf("请输入指定坐标:");
+////	scanf("%d %d",&x3,&y3);
+////	a = sqrt((x2 - x1)*(x2 - x1) + (y2 - y1) * (y2 - y1));
+////	b = sqrt((x3 - x1)*(x3 - x1) + (y3 - y1) * (y3 - y1));
+////	c = sqrt((x2 - x3)*(x2 - x3) + (y2 - y3) * (y2 - y3));
+////	k1 = (y2 - y1) / (x2 - x1);
+////	k2 = (y3 - y2) / (x3 - x2);
+////	k3 = (y3 - y1) / (x3 - x1);
+////	q = (a + b + c)/2;
+////	S = sqrt(q * (q - a)*(q - b)*(q - c));
+////	if (k1 = k2)
+////	{
+////		if (k2 != k3) 
+////		{
+////			printf("三角形的面积为%lf\n", S);
+////		}
+////		else
+////			printf("不能组成一个三角形\n");
+////	}
+////	else
+////	{
+////		printf("三角形的面积为%lf\n", S);
+////	}
+////	system("pause");
+////	return 0;
+////}
+////
+////
+////int main()
+////{
+////	float r, s, l;
+////	int i;
+////	printf("*****************************\n1------计算圆的面积\n2------计算圆的周长\n3------计算圆的周长和面积\n******************************\n请选择（1--3）：");
+////	scanf("%d", &i);
+////	printf("请输入圆的半径：");
+////	scanf("%f", &r);
+////	if (1 == i)
+////	{
+////		printf("圆的面积是：%f", 3.14*r*r);
+////	}
+////	if (2 == i)
+////	{
+////		printf("圆的周长是：%f", 3.14*2*r);
+////	}
+////	if (3 == i)
+////	{
+////		printf("圆的面积和周长是：%f和%f", 3.14*r*r, 3.14 * 2 * r);
+////	}
+////	system("pause");
+////	return 0;
+////}
+////
+////
+////
+////
+////int main() {
+////	int x, y;
+////	char op;
+////	scanf("%d %c %d", &x, &op, &y);
+////	switch (op)
+////	{
+////	case  '+':
+////		printf("=%d", x + y);break;
+////	case  '-':
+////		printf("=%d", x - y);break;
+////	case'*':
+////		printf("=%d", x*y); break;
+////	case'/':
+////		if (y=0)
+////			printf("Error!");  
+////        else { printf("=%d", x / y); 
+////		};
+////		break;
+////	default:
+////		printf("error");
+////	}
+////	system("pause");
+////	return 0;
+////}
+//
+//int main() {
+//	int a = 0;
+//	scanf("请输入一个数%d", &a);
+//	if (a % 2 == 0)
+//		printf("偶数\n");
+//	else
+//		printf("奇数\n");
+//	system("pause");
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int a, b, c, t;
+//	scanf("%d %d %d", &a, &b, &c);
+//	if (a < b)
+//	{
+//		t = a;
+//		a = b;
+//		b = t;
+//	}
+//	if (a < c)
+//	{
+//		t = a;
+//		a = c;
+//		c = t;
+//	}
+//	if (b < c)
+//	{
+//		t = b;
+//		b = c;
+//		c = t;
+//	}
+//	printf("%d", c);
+//	system("pause");
+//	return 0;
+////}
+////
+//int main() {
+//	int a, b, c, t;
+//	scanf("%d %d %d", &a, &b, &c);
+//	if (a < b)
+//	{
+//		t = a;
+//		a = b;
+//		b = t;
+//	}
+//	if (a < c)
+//	{
+//		t = a;
+//		a = c;
+//		c = t;
+//	}
+//	if (b < c)
+//	{
+//		t = b;
+//		b = c;
+//		c = t;
+//	}
+//	printf("%d %d %d", a, b, c);
+//	system("pause");
+//	return 0;
+//
+////}
+////
+////
+//int main() {
+//	int ch;
+//	ch = getche();
+//	if ((ch >= 97) && (ch <= 122))
+//		printf(" %c 是小写字母\n", ch);
+//	else if ((ch >= 65) && (ch <= 90))
+//		printf("\n%c是大写字母\n", ch);
+//	else if ((ch >= 48) && (ch <= 57))
+//		printf("\n%c是数字字符\n", ch);
+//	else
+//		printf("\n%c是其他字符\n", ch);
+//	system("pause");
+//	return 0;
+////
+////
+//	int main() {
+//		int p;
+//		scanf("请输入分数%d", &p);
+//		if ((p >= 90) && (p <= 100))
+//			printf("A");
+//		else if ((p >= 80) && (p <= 90))
+//			printf("B");
+//		else if ((p >= 70) && (p <= 80))
+//			printf("C");
+//		else if ((p >= 60) && (p <= 70))
+//			printf("D");
+//		else
+//			printf("E");
+//		system("pause");
+//		return 0;
+//	}
+
+//
+//int main() { 
+//	int i, k;
+//
+//	for (i = 0, k = -1; k = 1; i++, k++)
+//
+//		printf("*** ");
+//	system("pause");
+//	return 0;
+//}
